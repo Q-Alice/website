53191e0ea4650f6584a0285992873d7f7ca9dd62
k3y10 <k3yt3n@gmail.com>
2024-06-29 00:59:35 -0600

    attempting -f uninstall of scipy, sympy and symengine

--- remove_scipy.py
+++ remove_scipy.py
@@ -4,3 +4,5 @@ def uninstall_package(package_name):
     subprocess.check_call([f'pip uninstall -y {package_name}'], shell=True)
 
 uninstall_package('scipy')
+uninstall_package('sympy')
+uninstall_package('symengine')
